e734f6bfd8fa2105dd37cdc2a3396faeb184f3af
CG0323 <wangchun0323@qq.com>
2017-01-23 22:07:26 +0800

    add CORS support

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ var directories = require('./directories.js');
 var problems = require('./problems.js');
 var router = express.Router();
 
+router.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 router.use('/directories', directories);
 router.use('/problems', problems);
 
