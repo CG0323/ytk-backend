7a779ff65c5cfd19d4c8a4081cd7eb899edc8f22
CG0323 <wangchun0323@qq.com>
2017-02-13 19:06:54 +0800

    return user to client when login

--- routes/users.js
+++ routes/users.js
@@ -78,7 +78,7 @@ router.post('/login', function(req, res, next) {
         var expires = moment().add('days', 7).valueOf();
         var token = jwt.encode({ iss: { _id: user._id, role: user.role }, expires: expires }, secret);
         logger.info(user.name + " 登录系统。" + req.clientIP);
-        res.status(200).json({ token: token });
+        res.status(200).json({ name: user.name, username: user.username, role: user.role, token: token });
         // req.login(user, function(err) {
         //     if (err) {
         //         logger.error(err);
