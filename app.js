528d9cdb8a90b44fd5310d25c397499788ec3c25
CG0323 <wangchun0323@qq.com>
2017-02-14 18:49:40 +0800

    allow Authorization in header CORs

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(passport.initialize());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 app.use('/api', apis);
