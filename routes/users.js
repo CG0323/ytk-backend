35f8c0e142b86e5ab59def7b26789099d4b08f9c
CG0323 <wangchun0323@qq.com>
2017-02-14 21:41:43 +0800

    add 7 days expiration for token

--- routes/users.js
+++ routes/users.js
@@ -57,8 +57,8 @@ router.post('/login', function(req, res, next) {
         }
         var secret = config.token_secret;
         var expires = moment().add(7, 'days').valueOf();
-        // var token = jwt.encode({ iss: { _id: user._id, role: user.role }, expires: expires }, secret);
-        var token = jwts.encode({ _id: user._id, role: user.role }, secret);
+        var token = jwt.encode({ iss: { _id: user._id, role: user.role }, expires: expires }, secret);
+        // var token = jwts.encode({ _id: user._id, role: user.role }, secret);
         logger.info(user.name + " 登录系统。" + req.clientIP);
         res.status(200).json({ name: user.name, username: user.username, role: user.role, token: token });
 
