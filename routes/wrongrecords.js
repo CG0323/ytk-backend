fa699fab6c0dce60f8b96cca80b8776b5f692843
CG0323 <wangchun0323@qq.com>
2017-02-19 10:41:40 +0800

    modify wrong records api to accept a post to add/remove record

--- routes/wrongrecords.js
+++ routes/wrongrecords.js
@@ -6,23 +6,35 @@ var Q = require('q');
 
 router.post('/', function(req, res) {
     var data = req.body;
-    WrongRecord.find({ user: data.user, problem: data.problem })
-        .exec()
-        .then(function(records) {
-            if (records.length > 0) { //aready exist
-                res.status(200).json({ message: "一错再错..." })
-            } else {
-                var wrongRecord = new WrongRecord(data);
-                wrongRecord.save(function(err, savedRecord, numAffected) {
-                    if (err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.status(200).json({ _id: savedRecord._id });
-                    }
-                });
-            }
-        })
-
+    var correct = data.correct;
+    if (correct) { // remove from database if exist
+        WrongRecord.remove({ user: data.user, problem: data.problem })
+            .exec()
+            .then(function(data) {
+                    res.json({ message: "错题记录已成功删除" });
+                },
+                function(err) {
+                    res.status(500).json({ error: err, message: "错题记录删除失败" });
+                }
+            )
+    } else { // add to database if not exist
+        WrongRecord.find({ user: data.user, problem: data.problem })
+            .exec()
+            .then(function(records) {
+                if (records.length > 0) { //aready exist
+                    res.status(200).json({ message: "一错再错..." })
+                } else {
+                    var wrongRecord = new WrongRecord(data);
+                    wrongRecord.save(function(err, savedRecord, numAffected) {
+                        if (err) {
+                            res.status(500).send(err);
+                        } else {
+                            res.status(200).json({ _id: savedRecord._id });
+                        }
+                    });
+                }
+            })
+    }
 });
 
 // 临时接口
@@ -52,21 +64,5 @@ router.get('/user/:userId', function(req, res, next) {
             }
         )
 });
-
-
-
-router.delete('/:id', function(req, res, next) {
-    var id = req.params.id;
-    WrongRecord.remove({ _id: id })
-        .exec()
-        .then(function(data) {
-                res.json({ message: "错题记录已成功删除" });
-            },
-            function(err) {
-                res.status(500).json({ error: err, message: "错题记录删除失败" });
-            }
-        )
-});
-
 
 module.exports = router;
