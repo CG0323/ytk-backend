b1557fbf5f0d0ba0103cd992e448b6ac0b9be4fa
CG0323 <wangchun0323@qq.com>
2017-02-24 09:14:34 +0800

    fix path issue

--- routes/wechat.js
+++ routes/wechat.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
 var WechatAPI = require('wechat-api');
-var appConfig = require('../common.js').config();
 var OAuth = require('wechat-oauth');
 var db = require('../utils/database.js').connection;
 var logger = require('../utils/logger.js');
 var config = require('../common.js').config();
 
 
-var client = new OAuth(appConfig.app_id, appConfig.app_secret);
-var api = new WechatAPI(appConfig.app_id, appConfig.app_secret);
+var client = new OAuth(config.wechat.app_id, config.wechat.app_secret);
+var api = new WechatAPI(config.wechat.app_id, config.wechat.app_secret);
 
 router.get('/', wechat(config.wechat.token, function(req, res, next) {
     res.writeHead(200);
