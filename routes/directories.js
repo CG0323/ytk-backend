c3bb88dd998a9998f30c05731fef8c7ae391f178
CG0323 <wangchun0323@qq.com>
2017-02-27 13:11:57 +0800

    dbug# Please enter the commit message for your changes. Lines starting

--- routes/directories.js
+++ routes/directories.js
@@ -111,7 +111,7 @@ function getNode(directory, passed_directories) {
     var node = {};
     node.label = directory.name;
     if (directory.level == 3) {
-        if (passed_directories.indexOf(directory._id) >= 0) {
+        if (passed_directories.indexOf("58a44b410688275e9942d44f") >= 0) {
             console.log("heere");
             node.label = directory.name + "(已达标)";
         }
