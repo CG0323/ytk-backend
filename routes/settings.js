fdc376c992a0a27f98c696197f86795c24a93a6c
CG0323 <wangchun0323@qq.com>
2017-02-28 11:04:36 +0800

    add api to modify settings

--- routes/settings.js
+++ routes/settings.js
@@ -48,6 +48,43 @@ router.get('/', function(req, res, next) {
         )
 });
 
+router.post('/', jwt({ secret: secretCallback }), function(req, res) {
+    var user = req.user;
+    if (user.role != "管理员") {
+        res.status(401).json({ message: "无权限修改系统配置" });
+    }
+
+    var new_settings = req.body;
+    Settings.find()
+        .exec()
+        .then(function(data) {
+                var setting;
+                if (data.length === 0) { // db is empty, create the first one
+                    settings = new Settings(new_settings);
+                } else { // update existing one
+                    settings = data[0];
+                    settings.price_3_months = new_settings.price_3_months;
+                    settings.price_12_months = new_settings.price_12_months;
+                    settings.trial_days = new_settings.trial_days;
+                    settings.exam_duration = new_settings.exam_duration;
+                    settings.score_per_turn = new_settings.score_per_turn;
+                    settings.time_bonus_per_second = new_settings.time_bonus_per_second;
+                    settings.default_pass_score = new_settings.default_pass_score;
+                }
+                settings.save(function(err, savedSettings, numAffected) {
+                    if (err) {
+                        res.status(500).json({ message: err });
+                    } else {
+                        res.status(200).json({ message: "系统配置已成功保存" });
+                    }
+                });
+            },
+            function(err) {
+                res.status(500).json({ message: err });
+            }
+        )
+});
+
 function getDefaultSettings() {
     var settings = {};
     settings.price_3_months = 10;
