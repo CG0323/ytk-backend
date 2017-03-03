774803d7dd428ee58e14cda6a3bbba6c3af6bb18
CG0323 <wangchun0323@qq.com>
2017-03-03 15:36:15 +0800

    add order apis

--- routes/orders.js
+++ routes/orders.js
@@ -6,16 +6,100 @@ var jwt = require('express-jwt');
 var config = require('../common.js').config();
 var logger = require('../utils/logger.js');
 var secretCallback = require('../utils/secretCallback.js').secretCallback;
+var Order = require('../models/order')(db);
 
 router.post('/prepare', jwt({ secret: secretCallback }), function(req, res) {
     var tradeNo = generateOutTradeNo();
     res.status(200).json({ tradeNo: tradeNo });
 });
 
-router.get('/wxpay/notify1/:total', function(req, res) {
-    var total = req.params.total;
-    res.status(200).send("您的" + total + "元已成功支付。");
+router.get('/wxpay/notify1/:tradeNo', function(req, res) {
+    var tradeNo = req.params.tradeNo;
+    // create the order in db
+    var order = new Order();
+    order.out_trade_no = tradeNo;
+    order.save(function(err, savedOrder, numAffected) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ message: err });
+        } else {
+            res.status(200).send("您的订单：" + tradeNo + "已成功支付。");
+        }
+    });
 })
+
+router.get('/query/:tradeNo', jwt({ secret: secretCallback }), function(req, res) {
+    var tradeNo = req.params.tradeNo;
+    Order.find({ out_trade_no: tradeNo })
+        .exec()
+        .then(function(orders) {
+                res.json(orders);
+            },
+            function(err) {
+                res.status(500).json({ message: err });
+            }
+        )
+})
+
+router.post('/', jwt({ secret: secretCallback }), function(req, res) {
+    var updated_order = req.body;
+    Order.find({ out_trade_no: updated_order.out_trade_no })
+        .exec()
+        .then(function(data) {
+                var setting;
+                if (data.length === 0) { // db is empty, create the first one
+                    res.status(500).json({ message: "该订单尚未完成支付" });
+                } else { // update existing one
+                    order = data[0];
+                    order.user = req.user.iss;
+                    order.tansaction_id = "1234567890123";
+                    order.order_date = new Date();
+                    order.total_fee = updated_order.total_fee;
+                    order.package = updated_order.package;
+                    order.student_usernames = updated_order.student_usernames;
+                    order.student_names = updated_order.student_names;
+                    order.save(function(err, savedOrder, numAffected) {
+                        if (err) {
+                            res.status(500).json({ message: err });
+                        } else {
+                            res.status(200).json({ message: "订单信息已成功保存" });
+                        }
+                    });
+                }
+            },
+            function(err) {
+                res.status(500).json({ message: err });
+            }
+        )
+});
+
+//临时接口
+router.get('/clear', function(req, res, next) {
+    Order.find()
+        .remove()
+        .exec()
+        .then(function(orders) {
+                res.json(orders);
+            },
+            function(err) {
+                res.status(500).end();
+            }
+        )
+});
+
+//临时接口
+router.get('/', function(req, res, next) {
+    var query = {};
+    Order.find(query)
+        .exec()
+        .then(function(orders) {
+                res.json(orders);
+            },
+            function(err) {
+                res.status(500).json({ message: err });
+            }
+        )
+});
 
 
 
